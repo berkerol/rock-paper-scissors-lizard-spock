2e406b28dd025aa15354851b0fe587be34b28042
Berk Erol <berk.erol@boun.edu.tr>
2020-01-25 12:47:39 +0300

    Merge pages with query parameter

--- index.md
+++ index.md
@@ -1,3 +1,3 @@
 # Rock Paper Scissors Lizard Spock
 
-Remake of the classic game with the Lizard Spock expansion. Computer chooses its option randomly. You can play [with](rpsls.html) or [without](rps.html) the Lizard Spock expansion.
+Remake of the classic game with the Lizard Spock expansion. Computer chooses its option randomly. You can play [with](rpsls.html?lizard-spock) or [without](rpsls.html) the Lizard Spock expansion.
--- rpsls.js
+++ rpsls.js
@@ -1,24 +1,31 @@
-window.create = function (n) {
-  const photos = document.getElementById('photos');
-  const list = ['rock', 'paper', 'scissors', 'lizard', 'spock'];
-  for (let i = 0; i < n; i++) {
-    const img = document.createElement('img');
-    img.className = 'img-fluid rounded mx-auto d-block';
-    img.src = `images/${list[i]}.jpg`;
-    img.setAttribute('onclick', `fight(${i}, ${n})`);
-    const div = document.createElement('div');
-    div.className = 'col-6 col-md-4 mt-4';
-    div.appendChild(img);
-    photos.appendChild(div);
-  }
-};
+/* global write */
+const wonText = ', you won!';
+const lostText = ', you lost!';
+const wonAlert = 'alert-success';
+const lostAlert = 'alert-danger';
+const names = ['rock', 'paper', 'scissors', 'lizard', 'spock'];
+const photos = document.getElementById('photos');
+
+if (!new URL(window.location.href).searchParams.has('lizard-spock')) {
+  names.length = 3;
+}
+for (const i in names) {
+  const img = document.createElement('img');
+  img.className = 'img-fluid rounded mx-auto d-block';
+  img.src = `images/${names[i]}.jpg`;
+  img.setAttribute('onclick', `fight(${i})`);
+  const div = document.createElement('div');
+  div.className = 'col-6 col-md-4 mt-4';
+  div.appendChild(img);
+  photos.appendChild(div);
+}
 
 let totalWon = 0;
 let totalTie = 0;
 let totalLost = 0;
 
-window.fight = function (user, range) {
-  const cmp = Math.floor(Math.random() * range);
+window.fight = function (user) {
+  const cmp = Math.floor(Math.random() * names.length);
   let text = 'Computer chose ';
   switch (cmp) {
     case 0:
@@ -36,90 +43,86 @@ window.fight = function (user, range) {
     default:
       text += 'Spock';
   }
-  const won = ', you won!';
-  const lost = ', you lost!';
-  const wonAlert = 'alert-success';
-  const lostAlert = 'alert-danger';
   let className = 'alert ';
   text += '.<br>';
   if (user === 2 && cmp === 1) {
-    text += 'Scissors cuts Paper' + won;
+    text += 'Scissors cuts Paper' + wonText;
     className += wonAlert;
     totalWon++;
   } else if (user === 1 && cmp === 0) {
-    text += 'Paper covers Rock' + won;
+    text += 'Paper covers Rock' + wonText;
     className += wonAlert;
     totalWon++;
   } else if (user === 0 && cmp === 3) {
-    text += 'Rock crushes Lizard' + won;
+    text += 'Rock crushes Lizard' + wonText;
     className += wonAlert;
     totalWon++;
   } else if (user === 3 && cmp === 4) {
-    text += 'Lizard poisons Spock' + won;
+    text += 'Lizard poisons Spock' + wonText;
     className += wonAlert;
     totalWon++;
   } else if (user === 4 && cmp === 2) {
-    text += 'Spock smashes Scissors' + won;
+    text += 'Spock smashes Scissors' + wonText;
     className += wonAlert;
     totalWon++;
   } else if (user === 2 && cmp === 3) {
-    text += 'Scissors decapitates Lizard' + won;
+    text += 'Scissors decapitates Lizard' + wonText;
     className += wonAlert;
     totalWon++;
   } else if (user === 3 && cmp === 1) {
-    text += 'Lizard eats Paper' + won;
+    text += 'Lizard eats Paper' + wonText;
     className += wonAlert;
     totalWon++;
   } else if (user === 1 && cmp === 4) {
-    text += 'Paper disproves Spock' + won;
+    text += 'Paper disproves Spock' + wonText;
     className += wonAlert;
     totalWon++;
   } else if (user === 4 && cmp === 0) {
-    text += 'Spock vaporizes Rock' + won;
+    text += 'Spock vaporizes Rock' + wonText;
     className += wonAlert;
     totalWon++;
   } else if (user === 0 && cmp === 2) {
-    text += 'Rock crushes Scissors' + won;
+    text += 'Rock crushes Scissors' + wonText;
     className += wonAlert;
     totalWon++;
   } else if (user === 2 && cmp === 0) {
-    text += 'Rock crushes Scissors' + lost;
+    text += 'Rock crushes Scissors' + lostText;
     className += lostAlert;
     totalLost++;
   } else if (user === 0 && cmp === 4) {
-    text += 'Spock vaporizes Rock' + lost;
+    text += 'Spock vaporizes Rock' + lostText;
     className += lostAlert;
     totalLost++;
   } else if (user === 4 && cmp === 1) {
-    text += 'Paper disproves Spock' + lost;
+    text += 'Paper disproves Spock' + lostText;
     className += lostAlert;
     totalLost++;
   } else if (user === 1 && cmp === 3) {
-    text += 'Lizard eats Paper' + lost;
+    text += 'Lizard eats Paper' + lostText;
     className += lostAlert;
     totalLost++;
   } else if (user === 3 && cmp === 2) {
-    text += 'Scissors decapitates Lizard' + lost;
+    text += 'Scissors decapitates Lizard' + lostText;
     className += lostAlert;
     totalLost++;
   } else if (user === 2 && cmp === 4) {
-    text += 'Spock smashes Scissors' + lost;
+    text += 'Spock smashes Scissors' + lostText;
     className += lostAlert;
     totalLost++;
   } else if (user === 4 && cmp === 3) {
-    text += 'Lizard poisons Spock' + lost;
+    text += 'Lizard poisons Spock' + lostText;
     className += lostAlert;
     totalLost++;
   } else if (user === 3 && cmp === 0) {
-    text += 'Rock crushes Lizard' + lost;
+    text += 'Rock crushes Lizard' + lostText;
     className += lostAlert;
     totalLost++;
   } else if (user === 0 && cmp === 1) {
-    text += 'Paper covers Rock' + lost;
+    text += 'Paper covers Rock' + lostText;
     className += lostAlert;
     totalLost++;
   } else if (user === 1 && cmp === 2) {
-    text += 'Scissors cuts Paper' + lost;
+    text += 'Scissors cuts Paper' + lostText;
     className += lostAlert;
     totalLost++;
   } else {
@@ -128,9 +131,5 @@ window.fight = function (user, range) {
     totalTie++;
   }
   document.getElementById('statistics').innerHTML = `Won: ${totalWon} Tie: ${totalTie} Lost: ${totalLost}`;
-  const child = document.createElement('div');
-  child.className = className + ' alert-dismissible fade show';
-  child.innerHTML = '<button type="button" class="close" data-dismiss="alert"><span>&times;</span></button>' + text;
-  const parent = document.getElementById('text');
-  parent.insertBefore(child, parent.firstChild);
+  write(className, text);
 };
